ffc811826bba8c0b7290fa305a25f441356eee94
Brandon Karchewski <karcheba@mcmaster.ca>
2011-05-30 11:52:35 -0400

    v1.0.0.3

--- SlopeFEA/Slope/engine/CircularSurface.cs
+++ SlopeFEA/Slope/engine/CircularSurface.cs
@@ -1,16 +1,36 @@
-﻿using System;
+﻿/************************************************************************
+ * PROJECT:     SlopeFEA (c) 2011 Brandon Karchewski
+ *              Licensed under the Academic Free License version 3.0
+ *                  http://www.opensource.org/licenses/afl-3.0.php
+ * 
+ * CONTACT:     Brandon Karchewski
+ *              Department of Civil Engineering
+ *              McMaster University, JHE-301
+ *              1280 Main St W
+ *              Hamilton, Ontario, Canada
+ *              L8S 4L7
+ *              p: 905-525-9140 x24287
+ *              f: 905-529-9688
+ *              e: karcheba@mcmaster.ca
+ *              
+ * 
+ * SOURCE INFORMATION:
+ * 
+ * The repository for this software project is hosted on git at:
+ *      
+ *      git://github.com/karcheba/SlopeFEA
+ *      
+ * As such, the code for the project is free and open source.
+ * The relevant license is AFLv3 (see link above). See the
+ * README file in the root directory of the repository for a
+ * detailed project description, acknowledgements, references,
+ * and the revision history.
+ ************************************************************************/
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Shapes;
-using System.Windows.Controls.Primitives;
-using System.IO;
-using System.Threading;
-using System.ComponentModel;
 
 namespace SlopeFEA
 {
--- SlopeFEA/Slope/main/ClosableHeader.xaml.cs
+++ SlopeFEA/Slope/main/ClosableHeader.xaml.cs
@@ -1,9 +1,33 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
+﻿/************************************************************************
+ * PROJECT:     SlopeFEA (c) 2011 Brandon Karchewski
+ *              Licensed under the Academic Free License version 3.0
+ *                  http://www.opensource.org/licenses/afl-3.0.php
+ * 
+ * CONTACT:     Brandon Karchewski
+ *              Department of Civil Engineering
+ *              McMaster University, JHE-301
+ *              1280 Main St W
+ *              Hamilton, Ontario, Canada
+ *              L8S 4L7
+ *              p: 905-525-9140 x24287
+ *              f: 905-529-9688
+ *              e: karcheba@mcmaster.ca
+ *              
+ * 
+ * SOURCE INFORMATION:
+ * 
+ * The repository for this software project is hosted on git at:
+ *      
+ *      git://github.com/karcheba/SlopeFEA
+ *      
+ * As such, the code for the project is free and open source.
+ * The relevant license is AFLv3 (see link above). See the
+ * README file in the root directory of the repository for a
+ * detailed project description, acknowledgements, references,
+ * and the revision history.
+ ************************************************************************/
 
+using System.Windows.Controls;
 
 /// <summary>
 /// Interaction logic for ClosableHeader.xaml
